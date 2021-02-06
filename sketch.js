b04a965aec54eddcb63a40c9bb15f6a6c544109e
sjpriya <72385035+sjpriya@users.noreply.github.com>
2021-02-06 15:53:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,9 +8,6 @@ var database;
 
 var form, player, game;
 
-var cars, car1, car2, car3, car4;
-
-
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
   database = firebase.database();
