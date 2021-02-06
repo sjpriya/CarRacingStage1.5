868726f35f50360026e22b1ae8c7a0e9496164ac
sjpriya <72385035+sjpriya@users.noreply.github.com>
2021-02-06 15:56:13 +0530

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -29,11 +29,7 @@ class Game {
       form.display();
     }
 
-    car1 = createSprite(100,200);
-    car2 = createSprite(300,200);
-    car3 = createSprite(500,200);
-    car4 = createSprite(700,200);
-    cars = [car1, car2, car3, car4];
+    // add sprites for cars
   }
 
   play(){
@@ -59,17 +55,13 @@ class Game {
         x = x + 200;
         //use data form the database to display the cars in y direction
         y = displayHeight - allPlayers[plr].distance;
-        cars[index-1].x = x;
-        cars[index-1].y = y;
+        
 
-        if (index === player.index){
-          cars[index - 1].shapeColor = "red";
-          camera.position.x = displayWidth/2;
-          camera.position.y = cars[index-1].y
+        // highlight active player in red and set game camera accordingly
+        
         }
        
-        //textSize(15);
-        //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+      
       }
 
     }
